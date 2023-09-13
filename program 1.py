0d71268686e609dce70300ffc7f733de48ef9f83
Hardware <135996135+Bartosz098@users.noreply.github.com>
2023-09-13 21:49:44 +0200

    Testin learning file

---
+++ program 1.py
@@ -0,0 +1,14 @@
+# to podstawowy program
+x = 5.8
+z = 2
+print (x + z)
+a , b = 1 , 3
+print (a)
+print (b)
+testing = []
+testing.append(1)
+testing.append(3)
+
+print (testing[0])
+name = input("podaj imie: ")
+print("hello " + name)
